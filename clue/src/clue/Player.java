534c0f76fd0fe97c085d1a17d5c91ded45eb235c
DragonNinja386 <crazy338866@gmail.com>
2019-03-24 13:45:15 -0700

    Update Player.java

--- clue/src/clue/Player.java
+++ clue/src/clue/Player.java
@@ -1,1 +1,24 @@
+package clue;
 
+public class Player {
+  
+  private Info[] wInfo;
+  private Info[] pInfo;
+  private Info[] lInfo;
+  private Info room;
+  
+  public Player() {
+    
+   } 
+  
+  public Info getRoom() {
+    return room;
+   } 
+  
+  public boolean isInRoom() {
+    if (room != null) 
+      return false;
+    return true;
+   } 
+  
+ } 
